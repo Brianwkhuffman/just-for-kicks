07e2c54c8f18373712ed65e7722799940ec73c69
Briii <brianwkhuffman@gmail.com>
2019-06-19 18:01:36 -1000

    #8 done

--- js/app.js
+++ js/app.js
@@ -53,17 +53,46 @@ function thumbcount(event){
 let shoeprice = document.querySelector('#increase');
 shoeprice.addEventListener('click', priceraise);
 function priceraise(){
-    let x = document.getElementById('price4');
-    console.log(x);
-    console.log(x.childNodes)
-    x.childNodes[0].nodeValue++;
+    let x = document.querySelector('#price4');
+    //console.log(x);
+    //console.log(x.childNodes)
+    //x.childNodes[0].nodeValue++; increment by 1
+    x.childNodes[0].nodeValue = Number(x.childNodes[0].nodeValue) + 1000;
 }
 
 //6. Add an event to the minus icon that will decrement the price of the Air Jordan VI shoe each time the icon is clicked on.
+let shoeprice2 = document.querySelector('#decrease');
+shoeprice2.addEventListener('click', pricelower);
+function pricelower(){
+    let x = document.querySelector('#price5');
+    //x.childNodes[0].nodeValue--;
+    x.childNodes[0].nodeValue = Number(x.childNodes[0].nodeValue) - 1000;
+}
 
 //7. Add an event to the Air Jordan XI shoe that will show another colorway for that shoe after hovering over the image. 
+let airJordanBlock3 = document.querySelectorAll('.block3');
+let airPic = airJordanBlock3[1].childNodes[0];
+//console.log(airpic);
+airPic.addEventListener('mouseover', colorshow);
+
+function colorshow(){
+    if (airPic.src === "https://www.sneakerlegends.com/images/stories/virtuemart/product/378037623%20378037623%20378037623%20378037623.jpg"){
+    airPic.src = 'https://www.sneakerlegends.com/images/stories/virtuemart/product/resized/(BRAND%20NEW)%20Jordan%209%20Retro%20Bred%20Patent_295x295.jpg'
+    }else{
+        airPic.src = "https://www.sneakerlegends.com/images/stories/virtuemart/product/378037623%20378037623%20378037623%20378037623.jpg";
+    }
+    
+}
 
 //8. Add an event to the Air Jordan XII shoe that will increase the image by 50% after clicking on the image.
+let airJordanBlock1 = document.querySelectorAll('.block1');
+let airIncrease = airJordanBlock1[2].childNodes[0];
+airIncrease.addEventListener('click', increaseSize)
+
+function increaseSize(){
+    airIncrease.style.width = '375px';
+    airIncrease.style.height = '375px';
+}
 
 //9. Add events to both the plus and minus icon that will add or decrease the price each time the respective icon is clicked on.
 
